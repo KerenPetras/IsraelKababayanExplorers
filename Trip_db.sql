7c32dd6a11eac23b92fbcdff866ee513361805fb
KerenPetras <jackqlinep@gmail.com>
2023-04-03 18:30:07 +0300

    database creation

---
+++ Trip_db.sql
@@ -0,0 +1,4 @@
+	INSERT INTO trips (trip_name, trip_date, trip_time, available_seats, price,manager_id)
+VALUES ('Summer is here/Eilat Trip', '2023-04-15', '02:00:00', 60, 269.00,1),
+       ('Winter is coming/ Harmon trip', '2024-02-05', '01:00:00', 120, 179.00,1),
+       ('Cherry Picking Season / North Trip ', '2023-05-23', '08:00:00', 40, 149.00,1);
